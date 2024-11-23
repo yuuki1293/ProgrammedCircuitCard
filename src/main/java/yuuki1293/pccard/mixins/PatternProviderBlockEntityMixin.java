ca1666cd6c0cbac1eea49d03e86d9320439962dd
yuuki1293 <yuuki1293@outlook.jp>
2024-11-24 02:59:12 +0900

    implements IUpgradeableObject

---
+++ src/main/java/yuuki1293/pccard/mixins/PatternProviderBlockEntityMixin.java
@@ -0,0 +1,9 @@
+package yuuki1293.pccard.mixins;
+
+import appeng.api.upgrades.IUpgradeableObject;
+import appeng.blockentity.crafting.PatternProviderBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PatternProviderBlockEntity.class)
+public class PatternProviderBlockEntityMixin implements IUpgradeableObject {
+}
