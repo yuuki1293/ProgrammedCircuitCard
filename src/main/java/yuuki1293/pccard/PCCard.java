453c65f47aa13d6ff8d8f54f070b04c95b11d750
yuuki1293 <yuuki1293@outlook.jp>
2024-11-24 00:34:52 +0900

    replace example

--- src/main/java/yuuki1293/pccard/PCCard.java
+++ src/main/java/yuuki1293/pccard/PCCard.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package yuuki1293.pccard;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,11 +30,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(PCCard.MODID)
+public class PCCard
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "pccard";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public PCCard()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
