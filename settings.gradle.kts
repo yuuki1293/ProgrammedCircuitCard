b68e0932604d6dd91b107a9ee9ff16624dcefc57
yuuki1293 <yuuki1293@outlook.jp>
2025-10-28 15:51:11 +0900

    fix: project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,4 +16,4 @@ plugins {
     id ("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "ae2-pattern-encoding-access-terminal"
+rootProject.name = "Programmed Circuit Card"
