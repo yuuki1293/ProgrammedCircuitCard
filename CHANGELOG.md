a57309ee5d983fdf7910f54516afaea6d92f8ee6
yuuki1293 <yuuki1293@outlook.jp>
2026-07-25 00:40:24 +0900

    update version to v1.2.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # 1.20.1 Changelog
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/)
 
+## [1.2.11]
+### Added
+ - Expanded AE's Smart Blocking takes into account differences in Programmed Circuits. (by @Zatone0) [#56](https://github.com/yuuki1293/ProgrammedCircuitCard/issues/56)
+
 ## [1.2.10]
 ### Added
  - Supports Giga Pattern Provider (Expanded AE).
--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
         const val name = "Programmed Circuit Card"
         const val description = "Add a Programmed Circuit Card"
         const val license = "MIT"
-        const val version = "1.2.10"
+        const val version = "1.2.11"
         const val group = "yuuki1293.pccard"
         const val author = "yuuki1293"
         const val repositoryUrl = "https://github.com/yuuki1293/ProgrammedCircuitCard"
