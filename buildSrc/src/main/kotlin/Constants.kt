971c24c0323e98666b67595ef4a3b1a565b7690e
Nakamura Yuki <yuuki1293@outlook.jp>
2026-08-20 19:14:08 +0900

    update version to v1.3.2

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
         const val name = "Programmed Circuit Card"
         const val description = "Add a Programmed Circuit Card"
         const val license = "MIT"
-        const val version = "1.3.1"
+        const val version = "1.3.2"
         const val group = "yuuki1293.pccard"
         const val author = "yuuki1293"
         const val repositoryUrl = "https://github.com/yuuki1293/ProgrammedCircuitCard"
